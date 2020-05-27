01deb9a47dc9f6d1745a13195c58be43f7b469c1
Anthony N <antnat96@gmail.com>
2020-05-27 10:51:48 -0500

    base

---
+++ main.cpp
@@ -0,0 +1,24 @@
+/*
+* File:   main.cpp
+* Author: Anthony Natale CSIS 215
+*
+* Created on May 27, 2020
+* Implements the double threaded binary tree structure
+*/
+
+#include <string>
+#include <sstream>
+#include "BinNode.h"
+#include "book.h"
+#include "BST.h"
+#include "BSTNode.h"
+#include "dictionary.h"
+
+using namespace std;
+
+int main(int argc, char** argv) {
+
+	cout << "Anthony Natale -- CSIS 215 Programming Assignment 2 -- Double Threaded Binary Tree" << "\n" << endl;
+
+	return 0;
+}
