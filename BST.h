326644954a15a6f130922619567f547dee3abc83
Anthony N <antnat96@gmail.com>
2020-05-30 10:10:26 -0500

    formatting

--- BST.h
+++ BST.h
@@ -22,7 +22,6 @@ template <typename Key, typename E>
 class BST : public Dictionary<Key,E> {
 private:
   BSTNode<Key,E>* root;   // Root of the BST
-  
   int nodecount;         // Number of nodes in the BST
 
   // Private "helper" functions
@@ -44,70 +43,70 @@ public:
 		nodecount = 0; 
 	} 
   
-  //Note from Prof Sipantzi -- I've commented out the destructor
-  //since you would have to change clearhelp() to make it work with
-  //doubly-threaded trees and that is not part of the assignment.
-  //~BST() { clearhelp(root); }            // Destructor
+	//Note from Prof Sipantzi -- I've commented out the destructor
+	//since you would have to change clearhelp() to make it work with
+	//doubly-threaded trees and that is not part of the assignment.
+	//~BST() { clearhelp(root); }            // Destructor
 
-  void clear() // Reinitialize tree
-  {
-	  cout << "<------CLEARED------>" << endl; 
-	  clearhelp(root); 
-	  root = NULL; 
-	  nodecount = 0;
-  }
+	void clear() // Reinitialize tree
+	{
+		cout << "<------CLEARED------>" << endl; 
+		clearhelp(root); 
+		root = NULL; 
+		nodecount = 0;
+	}
 
-  // Insert a record into the tree.
-  // k Key value of the record.
-  // e The record to insert.
-  void insert(const Key& k, const E& e) {
-    root = inserthelp(root, k, e);
-    nodecount++;
-  }
+	// Insert a record into the tree.
+	// k Key value of the record.
+	// e The record to insert.
+	void insert(const Key& k, const E& e) {
+		root = inserthelp(root, k, e);
+		nodecount++;
+	}
 
-  // Remove a record from the tree.
-  // k Key value of record to remove.
-  // Return: The record removed, or NULL if there is none.
-  E* remove(const Key& k) {
-    E* temp = findhelp(root, k);   // First find it
-    if (temp != NULL) {
-      root = removehelp(root, k);
-      nodecount--;
-    }
-    return temp;
-  }
-  // Remove and return the root node from the dictionary.
-  // Return: The record removed, null if tree is empty.
-  E* removeAny() {  // Delete min value
-    if (root != NULL) {
-      E* temp = new E;
-      *temp = root->element();
-      root = removehelp(root, root->key());
-      nodecount--;
-      return temp;
-    }
-    else return NULL;
-  }
+	// Remove a record from the tree.
+	// k Key value of record to remove.
+	// Return: The record removed, or NULL if there is none.
+	E* remove(const Key& k) {
+		E* temp = findhelp(root, k);   // First find it
+		if (temp != NULL) {
+			root = removehelp(root, k);
+			nodecount--;
+		}
+		return temp;
+	}
+	// Remove and return the root node from the dictionary.
+	// Return: The record removed, null if tree is empty.
+	E* removeAny() {  // Delete min value
+		if (root != NULL) {
+			E* temp = new E;
+			*temp = root->element();
+			root = removehelp(root, root->key());
+			nodecount--;
+			return temp;
+		}
+		else return NULL;
+	}
 
-  // Return Record with key value k, NULL if none exist.
-  // k: The key value to find. */
-  // Return some record matching "k".
-  // Return true if such exists, false otherwise. If
-  // multiple records match "k", return an arbitrary one.
-  E* find(const Key& k) const { 
-	  return findhelp(root, k); 
-  }
+	// Return Record with key value k, NULL if none exist.
+	// k: The key value to find. */
+	// Return some record matching "k".
+	// Return true if such exists, false otherwise. If
+	// multiple records match "k", return an arbitrary one.
+	E* find(const Key& k) const { 
+		return findhelp(root, k); 
+	}
 
-  // Return the number of records in the dictionary.
-  int size() { 
-	  return nodecount; 
-  }
+	// Return the number of records in the dictionary.
+	int size() { 
+		return nodecount; 
+	}
 
-  void print() const { // Print the contents of the BST
-    if (root == NULL) cout << "The BST is empty.\n";
-    else printhelp(root, 0);
-  }
-  
+	// Print the contents of the BST
+	void print() const {
+	if (root == NULL) cout << "The BST is empty.\n";
+	else printhelp(root, 0);
+	}
 };
 
 // Visit -- prints out root
